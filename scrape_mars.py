c08d2b2c0d07228405a06fcbe2ecb3cdee20b2ac
tonitiri <taiwo@beyondbordersinvestment.com>
2019-10-15 13:53:40 -0500

    homework85%

--- scrape_mars.py
+++ scrape_mars.py
@@ -3,6 +3,8 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
+from IPython.display import HTML
 
 #Site Navigation
 def init_browser():
@@ -10,7 +12,6 @@ def init_browser():
     return Browser("chrome", **executable_path, headless=False)
 # Defining scrape & dictionary
 def scrape():
-    browser = init_browser()
     listings = {}
     listings["mars_news"] = mars_news()
     listings["mars_image"] = marsImage()
@@ -22,6 +23,7 @@ def scrape():
 
 # # NASA Mars News
 def mars_news():
+   browser = init_browser()
    news_url = "https://mars.nasa.gov/news/"
    browser.visit(news_url)
    html = browser.html
@@ -32,23 +34,29 @@ def mars_news():
    news_title = news.find("div", class_="content_title").text
 
    mars_news = [news_title, news_paragraph]
+   browser.quit()
    return mars_news
 
 
 
 # # JPL Mars Space Images - Featured Image
 def marsImage():
+   browser = init_browser()
    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
    browser.visit(url)
    html = browser.html
    soup = BeautifulSoup(html, "html.parser")
+   featured_image_url = soup.find_all('a', class_='button fancybox')
+   for link in featured_image_url: print("https://www.jpl.nasa.gov"+(link.get('data-fancybox-href')))
    image = (link.get('data-fancybox-href'))
    featured_image_url = "https://www.jpl.nasa.gov" + image
+   browser.quit()
    return featured_image_url
 
 
 # # Mars Weather
 def marsWeather():
+   browser = init_browser()
    url = "https://twitter.com/marswxreport?lang=en"
    browser.visit(url)
    html = browser.html
@@ -56,12 +64,12 @@ def marsWeather():
    mars_weather = soup.find("div",{"class":"js-tweet-text-container"})
    mars_weather = mars_weather.text.strip()
    mars_weather
+   browser.quit()
    return mars_weather
 
 # # Mars Facts
 def marsFacts():
-   import pandas as pd
-   from IPython.display import HTML
+   browser = init_browser()
    facts_url = "https://space-facts.com/mars/"
    browser.visit(facts_url)
    mars_data = pd.read_html(facts_url)
@@ -69,28 +77,55 @@ def marsFacts():
    mars_data.columns = ["Mars-Earth Comparison", "Mars", "Earth"]
    mars_data = mars_data.set_index("Mars-Earth Comparison")
    mars_facts = mars_data.to_html(index = True, header =True)
+   browser.quit()
    return mars_facts
 
 # # Mars Hemispheres
 def marsHem():
-   import time
+   browser = init_browser()
+   #import time
    hemispheres_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
    browser.visit(hemispheres_url)
    html = browser.html
    soup = BeautifulSoup(html, "html.parser")
-   mars_hemisphere = []
-   products = soup.find("div", class_ = "result-list" )
-   hemispheres = products.find_all("div", class_="item")
+   # mars_hemisphere = []
+   # products = soup.find("div", class_ = "result-list" )
+   # hemispheres = products.find_all("div", class_="item")
+   # for hemisphere in hemispheres:
+   #     title = hemisphere.find("h3").text
+   #     title = title.replace("Enhanced", "")
+   #     end_link = hemisphere.find("a")["href"]
+   #     image_link = "https://astrogeology.usgs.gov/" + end_link
+   #     browser.visit(image_link)
+   #     html = browser.html
+   #     soup=BeautifulSoup(html, "html.parser")
+   #     downloads = soup.find("div", class_="downloads")
+   #     image_url = downloads.find("a")["href"]
+   #     dictionary = {"title": title, "img_url": image_url}
+   #     mars_hemisphere.append(dictionary)
+   # browser.quit()
+   # return mars_hemisphere
+
+   hemispheres = soup.findAll("a",{"class":"itemLink"})
+   mars_hemispheres = []
+
    for hemisphere in hemispheres:
-       title = hemisphere.find("h3").text
-       title = title.replace("Enhanced", "")
-       end_link = hemisphere.find("a")["href"]
-       image_link = "https://astrogeology.usgs.gov/" + end_link
-       browser.visit(image_link)
-       html = browser.html
-       soup=BeautifulSoup(html, "html.parser")
-       downloads = soup.find("div", class_="downloads")
-       image_url = downloads.find("a")["href"]
-       dictionary = {"title": title, "img_url": image_url}
-       mars_hemisphere.append(dictionary)
-   return mars_hemisphere
+      try:
+            H = {'title':hemisphere.h3.text, 'url':'https://astrogeology.usgs.gov/'+hemisphere.get('href') }
+            browser.visit('https://astrogeology.usgs.gov/'+hemisphere.get('href'))
+            html= browser.html
+            soup=BeautifulSoup(html, "html.parser")
+
+            mars_hemispheres.append(H)
+
+            #Store list in MongoDB
+            #collection.insert_one(H)
+
+      except:
+         pass
+
+
+
+   #print (mars_hemispheres)
+   browser.quit()
+   return mars_hemispheres
